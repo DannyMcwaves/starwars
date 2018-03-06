a713f60d566bf0e06e482a686f5ea07d2b726eb7
dannymcwaves <dannymcwaves96@gmail.com>
2018-03-06 18:00:43 +0000

    app.js updated to remove extra deps

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 let express = require('express'),
     logger = require('morgan'),
-    cookie_parser = require("cookie-parser"),
-    method_override = require("method-override"),
     compression = require('compression'),
-    body_parser = require("body-parser"),
     errorhandler = require("errorhandler"),
-    session = require("express-session"),
     path = require("path"),
     routes = require("./routes/home"),
 
@@ -15,7 +11,7 @@ let express = require('express'),
 // configuring the application information: response formats, name
 // templates to use, the current mode of the app. ie; development || production
 // and other setting the app has to depend on.
-app.set("appName", "ats_frontend");
+app.set("appName", "swapis");
 app.set("x-powered-by", false);
 app.set("env", "production");
 app.set("view engine", "html");
@@ -25,19 +21,10 @@ app.set("json spaces", 4);
 app.set("jsonp callback name", "cb");
 
 
-// DEVELOPER MODE
-// configuring middlewares to parse the data on the request, // bodyParser
 // log the request // logger
 // compress the response //compression
-// provide response to all methods even non standard ones // method_override
-// use cookies and sessions in the requests and the response
 app.use(logger("dev"));
 app.use(compression({threshold: 1}));
-app.use(body_parser.json({strict: false, limit: 5000}));
-app.use(body_parser.urlencoded({limit: 10000, extended: true}));
-app.use(method_override("_method"));
-app.use(cookie_parser("this is used for constructing cookies for a particular project."));
-app.use(session({key: "_sessId", secret: "jkqhrqnuthvnwusviwivhguvovnewk", resave: true, saveUninitialized: true}));
 
 // configuring the middlewares routes to respond to STATIC GET REQUESTS from the client.
 // GET static contents from the server using this middleware setups.
